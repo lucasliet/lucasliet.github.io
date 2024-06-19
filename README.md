ee2e1ace69da8305616f7b1fb5b3ea517f469c32
Lucas Oliveira <lucasouliveira@gmail.com>
2024-06-19 00:26:56 -0300

    initial show repository config

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Github Repositories
+
+This repository serves as the central hub for showcasing some of my personal projects hosted on GitHub. Each project listed below has its own dedicated repository, along with a link to its respective homepage.
+
+## Contact
+
+You can reach out to me via information provided on my [GitHub Profile](https://github.com/lucasliet).
+
+## License
+
+This repository is licensed under the [LICENSE](LICENSE)
---
+++ script.js
@@ -0,0 +1,80 @@
+getOrFetch('https://api.github.com/users/lucasliet', renderUserPhoto);
+getOrFetch('https://api.github.com/users/lucasliet/repos', renderRepositories);
+
+function getOrFetch(url, renderFunction) {
+  const cachedData = JSON.parse(sessionStorage.getItem(url));
+
+  if (cachedData) {
+    renderFunction(cachedData);
+  } else {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) {
+          throw new Error(Object.values(data));
+        }
+        sessionStorage.setItem(url, JSON.stringify(data));
+        renderFunction(data);
+      })
+      .catch(error => {
+        const header = document.querySelector('main');
+        const errorMessage = document.createElement('p');
+        const errorMessageStyle = {
+          backgroundColor: 'red',
+          color: 'white',
+          fontWeight: 'bold',
+          padding: '1rem',
+          borderRadius: '5px',
+          fontSize: '3rem',
+        };
+        Object.assign(errorMessage.style, errorMessageStyle);
+        errorMessage.textContent = `An error occurred while fetching data from GitHub API | ${error}`;
+        header.appendChild(errorMessage);
+        console.error('Error:', error);
+      });
+  }
+}
+
+function renderUserPhoto(user) {
+  const header = document.querySelector('header');
+
+  const title = document.createElement('h1');
+  title.textContent = `${user.name}'s Repositories`;
+  header.appendChild(title);
+
+  const githubUrl = document.createElement('a');
+  githubUrl.href = user.html_url;
+  const userPhoto = document.createElement('img');
+  userPhoto.className = 'userphoto';
+  userPhoto.src = user.avatar_url;
+  userPhoto.alt = user.login;
+  userPhoto.href = user.html_url;
+  githubUrl.appendChild(userPhoto);
+  header.appendChild(githubUrl);
+
+}
+
+function renderRepositories(repositories) {
+  const repositoriesContainer = document.querySelector('main')
+  repositories
+    .filter(repository => repository.homepage)
+    .forEach(repository => {
+      const repositoryElement = document.createElement('div');
+      repositoryElement.className = 'repository';
+
+      const repositoryName = document.createElement('h3');
+      repositoryName.textContent = repository.name;
+      repositoryElement.appendChild(repositoryName);
+
+      const repositoryDescription = document.createElement('p');
+      repositoryDescription.textContent = repository.description;
+      repositoryElement.appendChild(repositoryDescription);
+
+      const repositoryLink = document.createElement('a');
+      repositoryLink.href = repository.homepage;
+      repositoryLink.textContent = 'Go to Homepage';
+      repositoryElement.appendChild(repositoryLink);
+
+      repositoriesContainer.appendChild(repositoryElement);
+    });
+}
